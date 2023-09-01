be25223b15a47c0e416c9e3e5a584420e0a18aba
CRISTIAN DAVID CUADROS ACOSTA <cristiancuadros284859@correo.itm.edu.co>
2023-09-01 13:08:18 -0500

    Branch1

---
+++ Investigadores/Investigadores.API/Data/DataContext.cs
@@ -0,0 +1,28 @@
+﻿using Investigadores.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Investigadores.API.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+
+            public DbSet<Cientifico> Cientificos { get; set; }
+
+            public DbSet<Colaborador> Colaboradores { get; set; }    
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+
+        {
+
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Cientifico>().HasIndex(c => c.Nombre).IsUnique();
+
+        }
+
+    }
+
+}
---
+++ Investigadores/Investigadores.Shared/Entities/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Investigadores.Shared.Entities
+{
+    public class Cientifico
+    {
+
+        public int Id { get; set; }
+
+        public string Nombre { get; set; } = null;
+
+        [Required(ErrorMessage = "El nombre es requerido")]
+        public string Apellido { get; set; } = null;
+
+        [Required(ErrorMessage = "El apellido es requerido")]
+        public string Afiliacion { get; set; } = null;
+
+        [Required(ErrorMessage = "Debe especificar su afiliación")]
+        public string Especializacion { get; set; } = null;
+
+        [Required(ErrorMessage = "Debe especificar su especialización")]
+        public string rol { get; set; } = null;
+
+    }
+    public class Colaborador
+    {
+        public string Id { get; set; }
+
+        public string Nombre { get; set; } = null;
+
+        [Required(ErrorMessage = "El nombre es requerido")]
+        public string Apellido { get; set; } = null;
+
+        [Required(ErrorMessage = "El apellido es requerido")]
+        public string Afiliacion { get; set; } = null;
+
+        [Required(ErrorMessage = "Debe especificar su afiliación")]
+        public string Especializacion { get; set; } = null;
+
+        [Required(ErrorMessage = "Debe especificar su especialización")]
+        public string rol { get; set; } = null;
+
+
+
+    }
+}
