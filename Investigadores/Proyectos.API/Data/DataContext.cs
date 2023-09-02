d4dcfae3fd78f214ec3f6b20a06d4fdc3ee36bd3
kevin <KevinPriso.ch@gmail.com>
2023-09-02 15:50:44 -0500

    Error 3:50pm

---
+++ Investigadores/Proyectos.API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Proyectos.Shared.Entities;
+
+namespace Proyectos.API.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Proyectos> proyectos { get; set; }
+    }
+}
---
+++ Investigadores/Proyectos.Shared/Entities/Proyecto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Proyectos.Shared.Entities
+{
+    internal class Proyecto
+    {
+        public int Id { get; set; } 
+
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "El nombre es requerido")]
+        public string fechaInicio { get; set; }
+        public string fechaFinal {  get; set; }
+        public string LiderProyecto { get; set; }
+        public string descripcion {  get; set; }
+        public string AreaInvestigacion { get; set; }
+    }
+}
